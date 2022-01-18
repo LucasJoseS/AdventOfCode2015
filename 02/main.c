1306f4805517d27b0f49fe728289b9cd67081981
LucasJSilva <lucasjsilva_0xf0@protonmail.com>
2022-01-18 13:54:55 -0300

    Second star, day two, year 2015

--- 02/main.c
+++ 02/main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int int_comp(const void* a, const void* b) {
+  int arg1 = *(const int*) a;
+  int arg2 = *(const int*) b;
+  
+  return (arg1 > arg2) - (arg1 < arg2);
+}
 
 int main() {
   FILE *fp = fopen("input", "r");
-  int l, w, h, lw, wh, hl, square, small, total = 0;
+  int total_paper_feet = 0;
+  int total_ribbon_feet = 0;
+  int s[3];   // surface
+  int ss[3];  // [0] = l * w
 
-  while(fscanf(fp, "%dx%dx%d", &l, &w, &h) != EOF) {
-    lw = l*w;
-    wh = w*h;
-    hl = h*l;
-    square = 2*lw + 2*wh + 2*hl;
+  while(fscanf(fp, "%dx%dx%d", &s[0], &s[1], &s[2]) != EOF) {
+    ss[0] = s[0] * s[1];
+    ss[1] = s[1] * s[2];
+    ss[2] = s[2] * s[0];
 
-    small = lw;
-    if(small > wh) small = wh;
-    if(small > hl) small = hl;
+    qsort(s, 3, sizeof(int), int_comp);
+    qsort(ss, 3, sizeof(int), int_comp);
 
-    total += small + square;
+    total_paper_feet += 2*ss[0] + 2*ss[1] + 2*ss[2] + ss[0];
+    total_ribbon_feet += 2*s[0] + 2*s[1] + s[0]*s[1]*s[2];
   }
 
-  printf("Total square feet of wrapping paper: %d\n", total);
+  printf("Total square feet of wrapping paper: %d\n", total_paper_feet);
+  printf("Toal of ribbon: %d\n", total_ribbon_feet);
 }
